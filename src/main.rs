b385129b77d9559be0c8d10610e4a52f77322de0
Abhishek Vidhate <abhishek.ai.dev@gmail.com>
2025-05-18 13:00:12 +0530

    rust mascot added in basic code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+// Basic Rust Program to print "Hello World" with Rust Mascot 
+
+use ferris_says::say;
+use std::io::{stdout, BufWriter};
+
 fn main() {
-    println!("Hello, welcome to Rust Development ^><^ !!!");
+    let stdout = stdout();
+    let message = String::from("Hello, welcome to Rust Development ^_* !!!");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(&message, width, &mut writer).unwrap();
 }
+
